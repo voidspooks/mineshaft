144c6479caefdc7b042e2b9565b1336356c11572
camerontesterman <camerontesterman@hotmail.com>
2017-04-14 13:20:53 -0500

    added copyright

--- lib/ruby-venv.rb
+++ lib/ruby-venv.rb
@@ -1,3 +1,11 @@
+# ruby-venv
+#
+# author:: Cameron Testerman
+# email:: cameronbtesterman@gmail.com
+# created:: 2017-04-14 1:19PM
+#
+# Copyright (c) 2017 Cameron Testerman
+
 require_relative "./ruby-venv/environment"
 require_relative "./ruby-venv/install"
 require_relative "./ruby-venv/version"
--- lib/ruby-venv/environment.rb
+++ lib/ruby-venv/environment.rb
@@ -1,3 +1,11 @@
+# ruby-venv
+#
+# author:: Cameron Testerman
+# email:: cameronbtesterman@gmail.com
+# created:: 2017-04-14 1:19PM
+#
+# Copyright (c) 2017 Cameron Testerman
+
 require 'fileutils'
 
 module RubyVenv

6611b203144a4ac25719e834621f038cbd9a8f80
camerontesterman <cameronbtesterman@gmail.com>
2018-09-20 22:24:54 -0500

    added env_list

---
+++ lib/mineshaft/env_list.rb
@@ -0,0 +1,14 @@
+# mineshaft
+#
+# author:: Cameron Testerman
+# email:: cameronbtesterman@gmail.com
+# created:: 2018-09-20 10:23PM
+#
+# Copyright (c) 2017-2018 Cameron Testerman
+
+module Mineshaft
+  def Mineshaft.env_list
+    # This should output a list of globally installed Rubies.
+    # The currently used environment should be highlighted.
+  end
+end
